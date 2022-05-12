dfb23ab8e78c92b112258143a0c9ba998182b7de
William46503 <52475057+William46503@users.noreply.github.com>
2022-05-12 23:38:06 +0900

    styled Card-roles text

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <a href="a" target="_blank">
+    <a href="a" target="_blank" class="card-container">
       <img src="https://placehold.jp/250x250.png" alt="Hero Image" />
       <h3>{{ hero.name }}</h3>
       <ul>
@@ -20,30 +20,49 @@ export default { props: ["hero"] };
   width: 180px;
   margin: 10px;
 
-  img {
-    width: 100%;
-    height: 150px;
-    margin-bottom: 10px;
+  .card-container {
+    position: relative;
 
-    object-fit: cover;
-    border-radius: 16px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-
-    transition: all 0.5s;
-  }
-
-  h3 {
-    text-decoration: none;
-    color: #313131;
-    font-size: 14px;
-    text-align: center;
-
-    transition: all 0.5s;
-  }
-
-  &:hover {
     img {
-      transform: scale(1.05);
+      width: 100%;
+      height: 150px;
+      margin-bottom: 10px;
+
+      object-fit: cover;
+      border-radius: 16px;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+
+      transition: all 0.5s;
+    }
+
+    h3 {
+      text-decoration: none;
+      color: #313131;
+      font-size: 14px;
+      text-align: center;
+
+      transition: all 0.5s;
+    }
+
+    ul {
+      position: absolute;
+      bottom: 100%;
+      left: 10px;
+      color: #fff;
+      opacity: 0;
+      font-size: 14px;
+      transition: all 0.5s;
+    }
+
+    &:hover {
+      img {
+        transform: scale(1.05);
+        height: 200px;
+      }
+
+      ul {
+        opacity: 1;
+      }
     }
   }
 }
