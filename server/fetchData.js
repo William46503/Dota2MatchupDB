2329f2411a3e9f13a6eafae8eb9ded2011af165e
William46503 <52475057+William46503@users.noreply.github.com>
2022-07-02 23:55:10 +0900

    Fetch data from OpenDota API and push the newest list to MongoDB

--- server/fetchData.js
+++ server/fetchData.js
@@ -2,20 +2,20 @@ const axios = require("axios");
 const mongoose = require("mongoose");
 const HeroModel = require("./models/Heroes");
 
+//To RUN: Right-click, run code.
+
 mongoose.connect(
   "mongodb+srv://nextlevelpenguin:MongoLlw46503@cluster0.khg9ypc.mongodb.net/Dota2App?retryWrites=true&w=majority"
 );
 
 //... <clustername>.khg9ypc.mongodb.net/<desired database/project name>?retryWrites=true&w=majority"
 
-
-
 const updatedHeroList = [];
 
 axios
   .get("https://api.opendota.com/api/heroes")
   .then(async function (response) {
-    const fetchedHeroList = response.data;
+    const fetchedHeroList = response.data; //Fetch newest data to update the herolist
     fetchedHeroList.forEach((item) => {
       updatedHeroList.push({
         id: item.id,
@@ -23,22 +23,17 @@ axios
         roles: item["roles"],
       });
     });
-
-    
   })
   .catch(function (err) {
     console.log(err);
   })
   .then(async function () {
-
+    //After succesfully fetching and proganized the listed of newest hero list, save to mongoose cloud DB
     updatedHeroList.forEach(async (item) => {
-        console.log(item);
-        const newHero = new HeroModel(item);
-        await newHero.save();
-      });
+      console.log(item);
+      const newHero = new HeroModel(item);
+      await newHero.save();
+    });
 
-    // HeroModel.insertMany(updatedHeroList, function(err) {
-    //     console.log(err);
-    // });
     console.log("Successfully fetched data from OpenDota");
   });
