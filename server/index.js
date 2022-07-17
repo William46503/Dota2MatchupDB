a0e1a19cd5babfe214460ca7adb095272cdbd8dc
William46503 <52475057+William46503@users.noreply.github.com>
2022-07-17 14:14:15 +0900

    Update index.js
    - Fixed Mongoose Uri
    - Functional /hero-data/search api-endpoint

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
+const dotenv = require("dotenv");
+require("dotenv").config();
+dotenv.config();
+
 const express = require("express");
 const app = express();
-require("dotenv").config();
+
 const mongoose = require("mongoose");
 const HeroModel = require("./models/Heroes");
 const { MatchupModel, MatchupDataModel } = require("./models/MatchupModel");
 
-mongoose.connect(
-  "mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.khg9ypc.mongodb.net/Dota2App?retryWrites=true&w=majority"
-);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}"@cluster0.khg9ypc.mongodb.net/Dota2App?retryWrites=true&w=majority`;
+mongoose
+  .connect(uri, {
+    serverSelectionTimeoutMS: 5000,
+  })
+  .catch((err) => console.log(err.reason));
 
+//Get all hero index
 app.get("/hero-index", (req, res) => {
   HeroModel.find((err, result) => {
     if (err) {
@@ -19,16 +27,18 @@ app.get("/hero-index", (req, res) => {
   });
 });
 
-// app.get("/hero-data/:heroId", (req, res) => {
-//   res.send(req.params);
-//   MatchupModel.find({ heroId: req.params["heroId"] }, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       res.json(result);
-//     }
-//   });
-// });
+//search for matchup data of a certain hero heroId : NUMBER
+app.get("/hero-data/search", (req, res) => {
+  let searchID = req.query.heroId;
+
+  MatchupModel.find({ heroId: searchID }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 app.listen(5000, () => {
   console.log("Port 5000 is up");
