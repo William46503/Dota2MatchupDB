2f3364f7e2cfbc21da5b74b5191247a0455255cd
William46503 <52475057+William46503@users.noreply.github.com>
2022-07-17 16:01:17 +0900

    Changed local port

--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,5 @@ import * as Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-// new Vue({
-//   render: (h) => h(App),
-// }).$mount("#app");
-
-// import the root component App from a single-file component.;
-
 const app = Vue.createApp(App);
 app.mount("#app");
